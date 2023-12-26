032ae4e14ba8ab78130be5066c37371f6fed437b
Eleazar Gamez <82742317+EleazarGamezD@users.noreply.github.com>
2023-12-26 18:47:11 -0500

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,22 +12,22 @@ import { JoiValidationSchema } from 'config/joi.validation';
 
 @Module({
   imports: [
-  ConfigModule.forRoot({   //linea de código para decirle a nest que lea el archivo de variable de entorno .ENV importante instalar @nestjs/config
-   load: [EnvConfiguration] , // mapeo de variables de entorno 
-   validationSchema: JoiValidationSchema, // validación del Schema de las variables de entorno instalar npm install  Joi
-  }),  
+    ConfigModule.forRoot({   //linea de código para decirle a nest que lea el archivo de variable de entorno .ENV importante instalar @nestjs/config
+      load: [EnvConfiguration], // mapeo de variables de entorno
+      validationSchema: JoiValidationSchema, // validación del Schema de las variables de entorno instalar npm install  Joi
+    }),
     ServeStaticModule.forRoot({
-    rootPath: join(__dirname,'..','public'),
-  }),
-  
-//MongooseModule.forRoot(process.env.MONGODB), // conexion a la base de datos Local en Docker 
-MongooseModule.forRoot(process.env.MONGODBATLAS), // aca es donde indicamos la url de conexion a la base de datos puede ser local o en mi caso por MongoAtlas "cloud"
- PokemonModule, 
- CommonModule, 
- SeedModule,
-],
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+
+    //MongooseModule.forRoot(process.env.MONGODB), // conexion a la base de datos Local en Docker
+    MongooseModule.forRoot(process.env.MONGODBATLAS), // aca es donde indicamos la url de conexion a la base de datos puede ser local o en mi caso por MongoAtlas "cloud"
+    PokemonModule,
+    CommonModule,
+    SeedModule,
+  ],
 })
 export class AppModule {
 
-  constructor(){  }
+  constructor() { }
 }
